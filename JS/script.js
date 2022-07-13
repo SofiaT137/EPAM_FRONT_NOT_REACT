c93d73339541991cb1b536fa83df3e9eaceec3e5
Sofiya Tkachenia <sofiya_tkachenia@epam.com>
2022-07-13 16:43:21 +0300

    Change jquery to js

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,11 @@
-$(document).ready(function() {
-    $('.headerBurger').click(function(event){
-        $('.headerBurger,.burgerMenu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
+const headerBurger = document.querySelector(".headerBurger");
+
+if (headerBurger) {
+const burgerMenu = document.querySelector(".burgerMenu");
+
+headerBurger.addEventListener("click", () => {
+    document.body.classList.toggle("lock");
+    burgerMenu.classList.toggle("active");
+    headerBurger.classList.toggle("active");
 });
+}
